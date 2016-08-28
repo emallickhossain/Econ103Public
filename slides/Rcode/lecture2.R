de226510cafa126923c128b0b3a9304788a55456
emallickhossain@gmail.com <emallickhossain@gmail.com>
2016-08-27 23:18:21 -0400

    Added code for regression

--- slides/Rcode/lecture2.R
+++ slides/Rcode/lecture2.R
@@ -44,3 +44,11 @@ y <- list(title = "Horsepower")
 plot_ly(data = carStats, x = carStats$mpg, y = carStats$hp, type = "scatter", mode = "markers") %>%
   layout(xaxis = x, yaxis = y, title = "Horsepower and MPG")
 
+# Basic Regression
+reg <- lm(hp ~ mpg, data = carStats)
+short_a <- round(reg$coefficients[1], digits = 2)
+short_b <- round(reg$coefficients[2], digits = 2)
+plot_ly(data = carStats, x = carStats$mpg, y = carStats$hp, type = "scatter", mode = "markers") %>%
+  add_trace(x = mpg, y = fitted(reg), mode = "lines") %>%
+  layout(xaxis = x, yaxis = y, title = "Horsepower and MPG", showlegend = FALSE)
+
